19ddbbb4d78c6c6b8f2c57e68255dc1790018795
SATYAM SHARMA <45912511+satyams2812@users.noreply.github.com>
2020-10-07 23:35:41 +0530

    Create multiplication.cpp

---
+++ Recursion/multiplication.cpp
@@ -0,0 +1,19 @@
+int multiplyNumbers(int m, int n) {
+  if(n<=0)
+  {
+      return 0;
+  }
+    
+    int ans = multiplyNumbers(m,n-1);
+     return ans+m;
+}
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+    cout << multiplyNumbers(m, n) << endl;
+}
